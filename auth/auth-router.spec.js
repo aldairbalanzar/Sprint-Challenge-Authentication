be9e9b09b1bc18e0ed89f0b6758474c52add6d3a
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-04-24 11:54:29 -0400

    setup endpoints and are working

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const server = require('../api/server');
+
+describe('server', () => {
+    const user = {
+        username: 'jest',
+        password: 'password'
+    }
+    
+    describe('POST /auth/register', () => {
+        it('status returns 201', () => {
+            return request(server)
+            .post('api/auth/register')
+            .send(user)
+            .then(res => {
+                console.log('201 TEST', res.status)
+                expect(res.status).toBe(201)
+            })
+        });
+
+        // it('returns message', () => {
+        //     return request(server)
+        //     .post('/api/auth/register')
+        //     .send(user)
+        //     .then(res => {
+        //         console.log('MESSAGE TEST: ', res.body)
+        //         expect(res.body.message).toBe('user registered')
+        //     })
+        // })
+    })
+})
