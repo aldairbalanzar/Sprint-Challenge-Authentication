90364dd4698563a8d1616d87bfee63a358f925c5
Aldair Balanzar <aldair.balanzar@outlook.com>
2020-04-24 14:21:29 -0400

    updated tests

--- jokes/jokes-router.spec.js
+++ jokes/jokes-router.spec.js
@@ -6,28 +6,23 @@ describe('server', () => {
         username: 'jest',
         password: 'password'
     }
-
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsInVzZXJuYW1lIjoicG9zdG1hbjIiLCJpYXQiOjE1ODc3NDk1MTksImV4cCI6MTU4NzgzNTkxOX0.qNX7AhF5sWKtTh6-x-xjuWWOGXn4-NjXA3sS5Z6vfpg'
     
     describe('GET /jokes', () => {
         it('status returns 200', () => {
             request(server)
             .get('/jokes')
-            .set('Authorization', token)
             .then(res => {
-                // console.log('200 TEST', res.status)
-                expect(res.header).toBe(true)
+                expect(res.body).toBeTruthy()
             })
         });
 
-        // it('returns message', () => {
-        //     request(server)
-        //     .post('auth/register')
-        //     .send(user)
-        //     .then(res => {
-        //         console.log('MESSAGE TEST: ', res.body)
-        //         expect(res.body.message).toBe('user registered')
-        //     })
-        // })
+        it('returns status 200', () => {
+            request(server)
+            .get('/jokes')
+            .then(res => {
+                expect(res).toBeTruthy()
+            })
+        })
     });
 })
